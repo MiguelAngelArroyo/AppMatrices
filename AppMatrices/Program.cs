71871e26e3451563ad3a7797eae2c728e985f2de
Miguel <arroyomiguel056@gmail.com>
2019-01-06 15:26:55 -0500

    Producto de matrices

--- AppMatrices/Program.cs
+++ AppMatrices/Program.cs
@@ -10,6 +10,36 @@ namespace AppMatrices
     {
         static void Main(string[] args)
         {
+            int[,] A = { { 1, 1 }, { 1, 1 }, { 1, 1 } };
+            int[,] B = { { 2, 2, 2 }, { 2, 2, 2 } };
+            int[,] resultado = new int[3, 3];
+            int c, suma;
+
+            for (int i = 0; i < 3; i++)
+            {
+                c = 0;
+                while(c<3)
+                {
+                    suma = 0;
+                    for(int j=0;j<2;j++)
+                    {
+                        suma = suma + (A[i, j] * B[j, c]);
+                        resultado[i, c] = suma;
+                        c = c + 1;
+                    }
+
+                }
+            }
+            for (int i=0;i<3;i++)
+            {
+                for(int j=0;j<3;j++)
+                {
+                    Console.WriteLine(resultado[i, j] + "\t");
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
